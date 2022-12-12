dff401ef05bf56879e78b0ca51df60bc634616ac
anatg <anatg@starkware.co>
2022-12-12 11:15:30 +0200

    Add test utils with get_test_instance for BlockHeader

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ pub mod core;
 pub mod hash;
 pub mod serde_utils;
 pub mod state;
+#[cfg(feature = "testing")]
+pub mod test_utils;
 pub mod transaction;
 
 use serde_utils::InnerDeserializationError;
