d9f3fd853704cec9721456bc6396fa3354caa64b
anatg <anatg@starkware.co>
2022-12-12 18:05:45 +0200

    Implement GetTestInstance with a proc_macro_derive

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,5 +1,4 @@
-use crate::block::{BlockHash, BlockHeader, BlockNumber, BlockTimestamp, GasPrice};
-use crate::core::{ContractAddress, GlobalRoot, PatriciaKey};
+use crate::core::PatriciaKey;
 use crate::hash::StarkHash;
 use crate::{patky, shash};
 
@@ -7,65 +6,32 @@ pub trait GetTestInstance: Sized {
     fn get_test_instance() -> Self;
 }
 
-auto_impl_get_test_instance! {
-    pub struct BlockHash(pub StarkHash);
-    pub struct BlockHeader {
-        pub block_hash: BlockHash,
-        pub parent_hash: BlockHash,
-        pub block_number: BlockNumber,
-        pub gas_price: GasPrice,
-        pub state_root: GlobalRoot,
-        pub sequencer: ContractAddress,
-        pub timestamp: BlockTimestamp,
-    }
-    pub struct BlockNumber(pub u64);
-    pub struct BlockTimestamp(pub u64);
-    pub struct GasPrice(pub u128);
-    pub struct GlobalRoot(pub StarkHash);
+////////////////////////////////////////////////////////////////////////
+// Implements the [`GetTestInstance`] trait for primitive types
+// that don't derive [`GetTestInstance`].
+////////////////////////////////////////////////////////////////////////
+impl_get_test_instance! {
     u64;
     u128;
 }
 
 #[macro_export]
-macro_rules! auto_impl_get_test_instance {
+macro_rules! impl_get_test_instance {
     () => {};
-    // Tuple structs (no names associated with fields) - one field.
-    ($(pub)? struct $name:ident($(pub)? $ty:ty); $($rest:tt)*) => {
-        impl GetTestInstance for $name {
-            fn get_test_instance() -> Self {
-                Self(<$ty>::get_test_instance())
-            }
-        }
-        auto_impl_get_test_instance!($($rest)*);
-    };
-    // Structs with public fields.
-    ($(pub)? struct $name:ident { $(pub $field:ident : $ty:ty ,)* } $($rest:tt)*) => {
-        impl GetTestInstance for $name {
-            fn get_test_instance() -> Self {
-                Self {
-                    $(
-                        $field: <$ty>::get_test_instance(),
-                    )*
-                }
-            }
-        }
-        auto_impl_get_test_instance!($($rest)*);
-    };
-    // Primitive types.
     ($name:ident; $($rest:tt)*) => {
         impl GetTestInstance for $name {
             fn get_test_instance() -> Self {
                 Self::default()
             }
         }
-        auto_impl_get_test_instance!($($rest)*);
+        impl_get_test_instance!($($rest)*);
     }
 }
-pub use auto_impl_get_test_instance;
+pub use impl_get_test_instance;
 
 ////////////////////////////////////////////////////////////////////////
-// Implements the [`GetTestInstance`] trait for starknet_api types not
-// supported by the macro [`auto_impl_get_test_instance`].
+// Implements the [`GetTestInstance`] trait for starknet_api types
+// that don't derive [`GetTestInstance`].
 ////////////////////////////////////////////////////////////////////////
 impl GetTestInstance for StarkHash {
     fn get_test_instance() -> Self {
@@ -73,8 +39,8 @@ impl GetTestInstance for StarkHash {
     }
 }
 
-impl GetTestInstance for ContractAddress {
+impl GetTestInstance for PatriciaKey {
     fn get_test_instance() -> Self {
-        Self(patky!("0x1"))
+        patky!("0x1")
     }
 }
