79b0eb1d856515a985438ed8e9b2d553f707dd8a
anatg <anatg@starkware.co>
2022-12-15 13:48:14 +0200

    Move all implementation into the macro

--- get_test_instance_macro_derive/src/lib.rs
+++ get_test_instance_macro_derive/src/lib.rs
@@ -1,42 +1,71 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse, DeriveInput};
+use syn::{parse, Data, DeriveInput, Field, Fields};
 
 #[proc_macro_derive(GetTestInstance)]
 pub fn get_test_instance_macro_derive(input: TokenStream) -> TokenStream {
     let ast: DeriveInput = parse(input).unwrap();
     let name = &ast.ident;
     match ast.data {
-        syn::Data::Struct(data) => match data.fields {
-            syn::Fields::Unnamed(fields) => {
-                let tys = fields.unnamed.iter().map(|f| f.ty.clone());
-                let gen = quote! {
-                    impl GetTestInstance for #name {
-                        fn get_test_instance() -> Self {
-                            Self(
-                                #(#tys::get_test_instance(), )*
-                            )
-                        }
+        Data::Struct(data) => {
+            let field_tokens =
+                data.fields.iter().map(|f| impl_get_test_instance_for_field(f.clone()));
+            let self_tokens = if let Fields::Unnamed(_) = data.fields {
+                quote! {
+                    Self(
+                        #(#field_tokens, )*
+                    )
+                }
+            } else {
+                quote! {
+                    Self {
+                        #(#field_tokens, )*
                     }
-                };
-                TokenStream::from(gen)
-            }
-            syn::Fields::Named(fields) => {
-                let idents = fields.named.iter().map(|f| f.ident.clone());
-                let tys = fields.named.iter().map(|f| f.ty.clone());
-                let gen = quote! {
-                    impl GetTestInstance for #name {
-                        fn get_test_instance() -> Self {
-                            Self {
-                                #(#idents: #tys::get_test_instance(), )*
-                            }
-                        }
-                    }
-                };
-                TokenStream::from(gen)
-            }
-            _ => panic!("Not supported yet."),
-        },
+                }
+            };
+            let gen = quote! {
+                impl GetTestInstance for #name {
+                    fn get_test_instance() -> Self {
+                        #self_tokens                    }
+                }
+            };
+            TokenStream::from(gen)
+        }
         _ => panic!("Not supported yet."),
     }
+}
+
+fn impl_get_test_instance_for_field(field: Field) -> proc_macro2::TokenStream {
+    let mut tokens = proc_macro2::TokenStream::new();
+    if let Some(ident) = field.ident {
+        tokens.extend(quote!(#ident: ));
+    }
+
+    let ty = field.ty;
+    if let syn::Type::Path(tp) = &ty {
+        if tp.path.segments.len() == 1 {
+            let type_name = tp.path.segments[0].ident.to_string();
+            // Primitive types.
+            if type_name.to_lowercase() == type_name {
+                tokens.extend(quote!(#ty::default()));
+                return tokens;
+            }
+
+            // StarkHash and StarkFelt.
+            if type_name == "StarkHash" || type_name == "StarkFelt" {
+                tokens.extend(quote!(crate::shash!("0x1")));
+                return tokens;
+            }
+
+            // PatriciaKey.
+            if type_name == "PatriciaKey" {
+                tokens.extend(quote!(crate::patky!("0x1")));
+                return tokens;
+            }
+        }
+    }
+
+    // Other.
+    tokens.extend(quote!(#ty::get_test_instance()));
+    tokens
 }
