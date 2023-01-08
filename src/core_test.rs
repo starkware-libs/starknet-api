2423ffb25847977bebc56dc8205661cb49c96bf7
Noa Oved <noa@starkware.co>
2023-01-09 00:58:08 +0200

    Add test to calculate_contract_address.

--- src/core_test.rs
+++ src/core_test.rs
@@ -1,7 +1,12 @@
 use assert_matches::assert_matches;
+use starknet_crypto::FieldElement;
 
-use crate::core::{PatriciaKey, StarknetApiError};
-use crate::hash::{StarkFelt, StarkHash};
+use crate::core::{
+    calculate_contract_address, ClassHash, ContractAddress, PatriciaKey, StarknetApiError,
+    CONTRACT_ADDRESS_PREFIX, L2_ADDRESS_UPPER_BOUND,
+};
+use crate::hash::{pedersen_hash_array, StarkFelt, StarkHash};
+use crate::transaction::{Calldata, ContractAddressSalt};
 use crate::{patricia_key, stark_felt};
 
 #[test]
@@ -32,4 +37,31 @@ fn patricia_key_macro() {
         )
         .unwrap()
     );
+}
+
+#[test]
+fn test_calculate_contract_address() {
+    let salt = ContractAddressSalt(stark_felt!(1337));
+    let class_hash = ClassHash::default();
+    let deployer_address = ContractAddress::default();
+    let constructor_calldata =
+        Calldata(vec![stark_felt!(60), stark_felt!(70), FieldElement::MAX.into()].into());
+
+    let actual_address =
+        calculate_contract_address(salt, class_hash, &constructor_calldata, deployer_address)
+            .unwrap();
+
+    let constructor_calldata_hash = pedersen_hash_array(&constructor_calldata.0);
+    let address = pedersen_hash_array(&[
+        StarkFelt::try_from(format!("0x{}", hex::encode(CONTRACT_ADDRESS_PREFIX)).as_str())
+            .unwrap(),
+        *deployer_address.0.key(),
+        salt.0,
+        class_hash.0,
+        constructor_calldata_hash,
+    ]);
+    let mod_address = FieldElement::from(address) % *L2_ADDRESS_UPPER_BOUND;
+    let expected_address = ContractAddress::try_from(StarkFelt::from(mod_address)).unwrap();
+
+    assert_eq!(actual_address, expected_address);
 }
