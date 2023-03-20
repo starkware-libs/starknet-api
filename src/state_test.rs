49d3adb3d30f35c370e6a4ef52169d8804857695
Gilad Chase <gilad@starkware.co>
2023-03-20 18:45:18 +0200

    Allow `EntryPointOffset` to be serde'd from int/string
    - In new releases offsets will be numbers, whereas in the past they were
      strings.
    - Simplified casting logic.
    - Remove `serde from=` since it isn't relevant.
    - Added a test for the `into` case.

--- src/state_test.rs
+++ src/state_test.rs
@@ -1,9 +1,24 @@
+use std::collections::HashMap;
+
+use serde_json::json;
+
 use crate::state::EntryPointOffset;
 
 #[test]
-fn entry_point_offset_from_str() {
+fn entry_point_offset_from_json_str() {
+    let data = r#"
+        {
+            "offset_1":  2 ,
+            "offset_2": "0x7b"
+        }"#;
+    let offsets: HashMap<String, EntryPointOffset> = serde_json::from_str(data).unwrap();
+
+    assert_eq!(EntryPointOffset(2), offsets["offset_1"]);
+    assert_eq!(EntryPointOffset(123), offsets["offset_2"]);
+}
+
+#[test]
+fn entry_point_offset_into_json_str() {
     let offset = EntryPointOffset(123);
-    let as_str: String = offset.into();
-    assert_eq!("0x7b", as_str);
-    assert_eq!(EntryPointOffset::try_from(as_str).unwrap(), offset);
+    assert_eq!(json!(offset), json!(123));
 }
