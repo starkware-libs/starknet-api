3e30db7c30a68f99dbe2a2f193e86c5ccdbfda9b
Dori Medini <dori@starkware.co>
2023-05-16 18:30:38 +0300

    Generic uint->X converter
    Signed-off-by: Dori Medini <dori@starkware.co>

---
+++ src/type_utils.rs
@@ -0,0 +1,16 @@
+#[macro_export]
+macro_rules! impl_from_for_small_uints {
+    ($struct_type:ty) => {
+        $crate::impl_from_for_small_uints!($struct_type, u8, u16, u32, u64);
+    };
+
+    ($struct_type:ty, $($small_uint_type:ty),+) => {
+        $(
+            impl From<$small_uint_type> for $struct_type {
+                fn from(x: $small_uint_type) -> Self {
+                    Self::from(u128::from(x))
+                }
+            }
+        )+
+    };
+}
