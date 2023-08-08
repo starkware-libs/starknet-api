4968a50127de3a8fd15fb65cda056fbb7c0221e5
Dan Brownstein <dan@starkware.co>
2023-08-08 16:51:31 +0300

    refactor: move tx execution status into tx output

--- src/block.rs
+++ src/block.rs
@@ -8,9 +8,7 @@ use serde::{Deserialize, Serialize};
 use crate::core::{ContractAddress, GlobalRoot};
 use crate::hash::StarkHash;
 use crate::serde_utils::{BytesAsHex, PrefixedBytesAsHex};
-use crate::transaction::{
-    Transaction, TransactionExecutionStatus, TransactionHash, TransactionOutput,
-};
+use crate::transaction::{Transaction, TransactionHash, TransactionOutput};
 
 /// A block.
 #[derive(Debug, Default, Clone, Eq, PartialEq, Hash, Deserialize, Serialize, PartialOrd, Ord)]
@@ -41,7 +39,6 @@ pub struct BlockBody {
     pub transactions: Vec<Transaction>,
     pub transaction_outputs: Vec<TransactionOutput>,
     pub transaction_hashes: Vec<TransactionHash>,
-    pub transaction_execution_statuses: Vec<TransactionExecutionStatus>,
 }
 
 /// The status of a [Block](`crate::block::Block`).
