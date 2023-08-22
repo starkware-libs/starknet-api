2fd8d70de0affefddbb268df30cbd49df4975a0e
Barak <barakb@starkware.co>
2023-08-22 12:19:31 +0300

    Add DataAvailabilityMode enum.

---
+++ src/data_availability.rs
@@ -0,0 +1,30 @@
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+use crate::hash::StarkFelt;
+
+#[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+pub enum DataAvailabilityMode {
+    L1 = 0,
+    L2 = 1,
+}
+
+#[derive(Debug, Error)]
+pub enum DataAvailabilityError {
+    #[error("Invalid data availability mode: {data_availability_mode}.")]
+    InvalidDataAvailabilityMode { data_availability_mode: StarkFelt },
+}
+
+impl TryFrom<StarkFelt> for DataAvailabilityMode {
+    type Error = DataAvailabilityError;
+
+    fn try_from(felt: StarkFelt) -> Result<Self, Self::Error> {
+        if felt == StarkFelt::from(0_u8) {
+            Ok(DataAvailabilityMode::L1)
+        } else if felt == StarkFelt::from(1_u8) {
+            Ok(DataAvailabilityMode::L2)
+        } else {
+            Err(DataAvailabilityError::InvalidDataAvailabilityMode { data_availability_mode: felt })
+        }
+    }
+}
