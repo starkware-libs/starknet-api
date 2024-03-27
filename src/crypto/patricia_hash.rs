f4876867331c1011307f45956ca1529fd9641546
Dori Medini <dori@starkware.co>
2024-03-27 19:27:01 +0200

    feat: use poseidon hash in patricia hash tree
    Signed-off-by: Dori Medini <dori@starkware.co>

--- src/crypto/patricia_hash.rs
+++ src/crypto/patricia_hash.rs
@@ -17,17 +17,17 @@
 //!
 //! Hash of a node depends on the number of edges coming out of it:
 //! - A leaf: The hash is the input value of its key.
-//! - A single edge: pedersen_hash(child_hash, edge_mark) + edge_length.
-//! - '0' and '1' edges: pedersen_hash(zero_child_hash, one_child_hash).
+//! - A single edge: hash(child_hash, edge_mark) + edge_length.
+//! - '0' and '1' edges: hash(zero_child_hash, one_child_hash).
 
 #[cfg(test)]
 #[path = "patricia_hash_test.rs"]
 mod patricia_hash_test;
 
 use bitvec::prelude::{BitArray, Msb0};
-use starknet_crypto::FieldElement;
+use starknet_types_core::felt::Felt;
 
-use crate::hash::{pedersen_hash, StarkFelt};
+use crate::hash::{HashFunction, StarkFelt};
 
 const TREE_HEIGHT: u8 = 64;
 type BitPath = BitArray<[u8; 8], Msb0>;
@@ -57,7 +57,7 @@ enum SubTreeSplitting {
 
 /// Calculates Patricia hash root on the given values.
 /// The values are keyed by consecutive numbers, starting from 0.
-pub fn calculate_root(values: Vec<StarkFelt>) -> StarkFelt {
+pub fn calculate_root(values: Vec<StarkFelt>, hash_function: &impl HashFunction) -> StarkFelt {
     if values.is_empty() {
         return StarkFelt::ZERO;
     }
@@ -66,7 +66,7 @@ pub fn calculate_root(values: Vec<StarkFelt>) -> StarkFelt {
         .zip(0u64..)
         .map(|(felt, idx)| Entry { key: idx.to_be_bytes().into(), value: felt })
         .collect();
-    get_hash(SubTree { leaves: &leaves[..], height: 0_u8 })
+    get_hash(SubTree { leaves: &leaves[..], height: 0_u8 }, hash_function)
 }
 
 // Recursive hash calculation. There are 3 cases:
@@ -74,38 +74,50 @@ pub fn calculate_root(values: Vec<StarkFelt>) -> StarkFelt {
 // - Edge: All the keys start with a longest common ('0's) prefix. NOTE: We assume that the keys are
 // a continuous range, and hence the case of '1's in the longest common prefix is impossible.
 // - Binary: Some keys start with '0' bit and some start with '1' bit.
-fn get_hash(sub_tree: SubTree<'_>) -> StarkFelt {
+fn get_hash(sub_tree: SubTree<'_>, hash_function: &impl HashFunction) -> StarkFelt {
     if sub_tree.height == TREE_HEIGHT {
         return sub_tree.leaves.first().expect("a leaf should not be empty").value;
     }
     match get_splitting(&sub_tree) {
-        SubTreeSplitting::CommonZerosPrefix(n_zeros) => get_edge_hash(sub_tree, n_zeros),
+        SubTreeSplitting::CommonZerosPrefix(n_zeros) => {
+            get_edge_hash(sub_tree, n_zeros, hash_function)
+        }
         SubTreeSplitting::PartitionPoint(partition_point) => {
-            get_binary_hash(sub_tree, partition_point)
+            get_binary_hash(sub_tree, partition_point, hash_function)
         }
     }
 }
 
 // Hash on a '0's sequence with the bottom sub tree.
-fn get_edge_hash(sub_tree: SubTree<'_>, n_zeros: u8) -> StarkFelt {
-    let child_hash =
-        get_hash(SubTree { leaves: sub_tree.leaves, height: sub_tree.height + n_zeros });
-    let child_and_path_hash = pedersen_hash(&child_hash, &StarkFelt::ZERO);
-    StarkFelt::from(FieldElement::from(child_and_path_hash) + FieldElement::from(n_zeros))
+fn get_edge_hash(
+    sub_tree: SubTree<'_>,
+    n_zeros: u8,
+    hash_function: &impl HashFunction,
+) -> StarkFelt {
+    let child_hash = get_hash(
+        SubTree { leaves: sub_tree.leaves, height: sub_tree.height + n_zeros },
+        hash_function,
+    );
+    let child_and_path_hash = hash_function.hash_pair(&child_hash, &StarkFelt::ZERO);
+    StarkFelt::from(Felt::from(&child_and_path_hash) + Felt::from(n_zeros))
 }
 
 // Hash on both sides: starts with '0' bit and starts with '1' bit.
 // Assumes: 0 < partition point < sub_tree.len().
-fn get_binary_hash(sub_tree: SubTree<'_>, partition_point: usize) -> StarkFelt {
-    let zero_hash = get_hash(SubTree {
-        leaves: &sub_tree.leaves[..partition_point],
-        height: sub_tree.height + 1,
-    });
-    let one_hash = get_hash(SubTree {
-        leaves: &sub_tree.leaves[partition_point..],
-        height: sub_tree.height + 1,
-    });
-    pedersen_hash(&zero_hash, &one_hash)
+fn get_binary_hash(
+    sub_tree: SubTree<'_>,
+    partition_point: usize,
+    hash_function: &impl HashFunction,
+) -> StarkFelt {
+    let zero_hash = get_hash(
+        SubTree { leaves: &sub_tree.leaves[..partition_point], height: sub_tree.height + 1 },
+        hash_function,
+    );
+    let one_hash = get_hash(
+        SubTree { leaves: &sub_tree.leaves[partition_point..], height: sub_tree.height + 1 },
+        hash_function,
+    );
+    hash_function.hash_pair(&zero_hash, &one_hash)
 }
 
 // Returns the manner the keys of a subtree are splitting: some keys start with '1' or all keys
