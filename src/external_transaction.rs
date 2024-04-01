e1f10e150ac5cf74928677e06b57062f7ea88da2
Mohammad Nassar <mohammad@starkware.co>
2024-04-01 12:42:28 +0300

    feat(transaction): implement external transactions

---
+++ src/external_transaction.rs
@@ -0,0 +1,160 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
+
+// use serde_repr::{Deserialize_repr, Serialize_repr};
+use crate::core::{ClassHash, CompiledClassHash, ContractAddress, Nonce};
+use crate::state::{EntryPoint, EntryPointType};
+use crate::transaction::{
+    AccountDeploymentData, Calldata, ContractAddressSalt, PaymasterData, ResourceBoundsMapping,
+    Tip, TransactionSignature, TransactionVersion,
+};
+
+/// An external transaction.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(untagged)]
+pub enum ExternalTransaction {
+    /// A declare transaction.
+    Declare(ExternalDeclareTransaction),
+    /// A deploy account transaction.
+    DeployAccount(ExternalDeployAccountTransaction),
+    /// An invoke transaction.
+    Invoke(ExternalInvokeTransaction),
+}
+
+// Each transaction type has a field called `type`. This field needs to be of a type that
+// serializes to/deserializes from a constant string.
+
+/// The type field of a deploy account transaction. This enum serializes/deserializes into a
+/// constant string.
+#[derive(Clone, Copy, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+pub enum DeployAccountType {
+    #[serde(rename = "DEPLOY_ACCOUNT")]
+    #[default]
+    DeployAccount,
+}
+
+/// The type field of an invoke transaction. This enum serializes/deserializes into a constant
+/// string.
+#[derive(Clone, Copy, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+pub enum InvokeType {
+    #[serde(rename = "INVOKE_FUNCTION")]
+    #[default]
+    Invoke,
+}
+
+/// The type field of a declare transaction. This enum serializes/deserializes into a constant
+/// string.
+#[derive(Clone, Copy, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+pub enum DeclareType {
+    #[serde(rename = "DECLARE")]
+    #[default]
+    Declare,
+}
+
+/// A deploy account transaction that can be added to Starknet through the Starknet gateway.
+/// It has a serialization format that the Starknet gateway accepts in the `add_transaction`
+/// HTTP method.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct ExternalDeployAccountTransactionV3 {
+    pub resource_bounds: ResourceBoundsMapping,
+    pub tip: Tip,
+    pub contract_address_salt: ContractAddressSalt,
+    pub class_hash: ClassHash,
+    pub constructor_calldata: Calldata,
+    pub nonce: Nonce,
+    pub signature: TransactionSignature,
+    pub nonce_data_availability_mode: DataAvailabilityMode,
+    pub fee_data_availability_mode: DataAvailabilityMode,
+    pub paymaster_data: PaymasterData,
+    pub version: TransactionVersion,
+    pub r#type: DeployAccountType,
+}
+
+/// A deploy account transaction that can be added to Starknet through the Starknet gateway.
+/// It has a serialization format that the Starknet gateway accepts in the `add_transaction`
+/// HTTP method.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(untagged)]
+pub enum ExternalDeployAccountTransaction {
+    V3(ExternalDeployAccountTransactionV3),
+}
+
+/// An invoke account transaction that can be added to Starknet through the Starknet gateway.
+/// The invoke is a V3 transaction.
+/// It has a serialization format that the Starknet gateway accepts in the `add_transaction`
+/// HTTP method.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct ExternalInvokeTransactionV3 {
+    pub resource_bounds: ResourceBoundsMapping,
+    pub tip: Tip,
+    pub calldata: Calldata,
+    pub sender_address: ContractAddress,
+    pub nonce: Nonce,
+    pub signature: TransactionSignature,
+    pub nonce_data_availability_mode: DataAvailabilityMode,
+    pub fee_data_availability_mode: DataAvailabilityMode,
+    pub paymaster_data: PaymasterData,
+    pub account_deployment_data: AccountDeploymentData,
+    pub version: TransactionVersion,
+    pub r#type: InvokeType,
+}
+
+/// An invoke transaction that can be added to Starknet through the Starknet gateway.
+/// It has a serialization format that the Starknet gateway accepts in the `add_transaction`
+/// HTTP method.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(untagged)]
+pub enum ExternalInvokeTransaction {
+    V3(ExternalInvokeTransactionV3),
+}
+
+/// A declare transaction of a Cairo-v1 contract class that can be added to Starknet through the
+/// Starknet gateway.
+/// It has a serialization format that the Starknet gateway accepts in the `add_transaction`
+/// HTTP method.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct ExternalDeclareTransactionV3 {
+    pub contract_class: ContractClass,
+    pub resource_bounds: ResourceBoundsMapping,
+    pub tip: Tip,
+    pub signature: TransactionSignature,
+    pub nonce: Nonce,
+    pub compiled_class_hash: CompiledClassHash,
+    pub sender_address: ContractAddress,
+    pub nonce_data_availability_mode: DataAvailabilityMode,
+    pub fee_data_availability_mode: DataAvailabilityMode,
+    pub paymaster_data: PaymasterData,
+    pub account_deployment_data: AccountDeploymentData,
+    pub version: TransactionVersion,
+    pub r#type: DeclareType,
+}
+
+/// A declare transaction that can be added to Starknet through the Starknet gateway.
+/// It has a serialization format that the Starknet gateway accepts in the `add_transaction`
+/// HTTP method.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(untagged)]
+pub enum ExternalDeclareTransaction {
+    V3(ExternalDeclareTransactionV3),
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+pub struct ContractClass {
+    #[serde(rename = "sierra_program")]
+    pub compressed_sierra_program: String,
+    pub contract_class_version: String,
+    pub entry_points_by_type: HashMap<EntryPointType, Vec<EntryPoint>>,
+    pub abi: String,
+}
+
+#[derive(Clone, Debug, Deserialize_repr, Eq, PartialEq, Serialize_repr)]
+#[repr(u8)]
+pub enum DataAvailabilityMode {
+    L1 = 0,
+    L2 = 1,
+}
