5458bdaa9b281e392e199ec4d1cdc24e11b34db8
Mohammad Nassar <mohammad@starkware.co>
2024-04-24 14:50:17 +0300

    feat(transaction): implement rpc transaction

--- src/external_transaction_test.rs
+++ src/external_transaction_test.rs
@@ -1,4 +1,3 @@
-use std::collections::BTreeMap;
 use std::sync::Arc;
 
 use rstest::rstest;
@@ -8,25 +7,26 @@ use crate::external_transaction::{
     ContractClass, DataAvailabilityMode, ExternalDeclareTransaction, ExternalDeclareTransactionV3,
     ExternalDeployAccountTransaction, ExternalDeployAccountTransactionV3,
     ExternalInvokeTransaction, ExternalInvokeTransactionV3, ExternalTransaction,
+    ResourceBoundsMapping,
 };
 use crate::hash::{StarkFelt, StarkHash};
 use crate::transaction::{
-    AccountDeploymentData, Calldata, ContractAddressSalt, PaymasterData, Resource, ResourceBounds,
-    ResourceBoundsMapping, Tip, TransactionSignature,
+    AccountDeploymentData, Calldata, ContractAddressSalt, PaymasterData, ResourceBounds, Tip,
+    TransactionSignature,
 };
 use crate::{contract_address, patricia_key, stark_felt};
 
-fn create_resource_bounds() -> ResourceBoundsMapping {
-    let mut map = BTreeMap::new();
-    map.insert(Resource::L1Gas, ResourceBounds { max_amount: 100, max_price_per_unit: 12 });
-    map.insert(Resource::L2Gas, ResourceBounds { max_amount: 58, max_price_per_unit: 31 });
-    ResourceBoundsMapping(map)
+fn create_resource_bounds_for_testing() -> ResourceBoundsMapping {
+    ResourceBoundsMapping {
+        l1_gas: ResourceBounds { max_amount: 100, max_price_per_unit: 12 },
+        l2_gas: ResourceBounds { max_amount: 58, max_price_per_unit: 31 },
+    }
 }
 
 fn create_declare_v3() -> ExternalDeclareTransaction {
     ExternalDeclareTransaction::V3(ExternalDeclareTransactionV3 {
         contract_class: ContractClass::default(),
-        resource_bounds: create_resource_bounds(),
+        resource_bounds: create_resource_bounds_for_testing(),
         tip: Tip(1),
         signature: TransactionSignature(vec![StarkFelt::ONE, StarkFelt::TWO]),
         nonce: Nonce(stark_felt!("0x1")),
@@ -41,7 +41,7 @@ fn create_declare_v3() -> ExternalDeclareTransaction {
 
 fn create_deploy_account_v3() -> ExternalDeployAccountTransaction {
     ExternalDeployAccountTransaction::V3(ExternalDeployAccountTransactionV3 {
-        resource_bounds: create_resource_bounds(),
+        resource_bounds: create_resource_bounds_for_testing(),
         tip: Tip::default(),
         contract_address_salt: ContractAddressSalt(stark_felt!("0x23")),
         class_hash: ClassHash(stark_felt!("0x2")),
@@ -56,7 +56,7 @@ fn create_deploy_account_v3() -> ExternalDeployAccountTransaction {
 
 fn create_invoke_v3() -> ExternalInvokeTransaction {
     ExternalInvokeTransaction::V3(ExternalInvokeTransactionV3 {
-        resource_bounds: create_resource_bounds(),
+        resource_bounds: create_resource_bounds_for_testing(),
         tip: Tip(50),
         calldata: Calldata(Arc::new(vec![stark_felt!("0x2000"), stark_felt!("0x1000")])),
         sender_address: contract_address!("0x53"),
