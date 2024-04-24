a327a51d9769a484838067498c081369e9d3814e
Mohammad Nassar <mohammad@starkware.co>
2024-04-24 14:50:17 +0300

    feat(transaction): implement rpc transaction

--- src/rpc_transaction_test.rs
+++ src/rpc_transaction_test.rs
@@ -1,32 +1,23 @@
-use std::collections::BTreeMap;
 use std::sync::Arc;
 
 use rstest::rstest;
 
 use crate::core::{ClassHash, CompiledClassHash, ContractAddress, Nonce, PatriciaKey};
-use crate::external_transaction::{
-    ContractClass, DataAvailabilityMode, ExternalDeclareTransaction, ExternalDeclareTransactionV3,
-    ExternalDeployAccountTransaction, ExternalDeployAccountTransactionV3,
-    ExternalInvokeTransaction, ExternalInvokeTransactionV3, ExternalTransaction,
-};
 use crate::hash::{StarkFelt, StarkHash};
+use crate::rpc_transaction::{
+    ContractClass, DataAvailabilityMode, RPCDeclareTransaction, RPCDeclareTransactionV3,
+    RPCDeployAccountTransaction, RPCDeployAccountTransactionV3, RPCInvokeTransaction,
+    RPCInvokeTransactionV3, RPCTransaction, ResourceBoundsMapping,
+};
 use crate::transaction::{
-    AccountDeploymentData, Calldata, ContractAddressSalt, PaymasterData, Resource, ResourceBounds,
-    ResourceBoundsMapping, Tip, TransactionSignature,
+    AccountDeploymentData, Calldata, ContractAddressSalt, PaymasterData, Tip, TransactionSignature,
 };
 use crate::{contract_address, patricia_key, stark_felt};
 
-fn create_resource_bounds() -> ResourceBoundsMapping {
-    let mut map = BTreeMap::new();
-    map.insert(Resource::L1Gas, ResourceBounds { max_amount: 100, max_price_per_unit: 12 });
-    map.insert(Resource::L2Gas, ResourceBounds { max_amount: 58, max_price_per_unit: 31 });
-    ResourceBoundsMapping(map)
-}
-
-fn create_declare_v3() -> ExternalDeclareTransaction {
-    ExternalDeclareTransaction::V3(ExternalDeclareTransactionV3 {
+fn create_declare_v3() -> RPCDeclareTransaction {
+    RPCDeclareTransaction::V3(RPCDeclareTransactionV3 {
         contract_class: ContractClass::default(),
-        resource_bounds: create_resource_bounds(),
+        resource_bounds: ResourceBoundsMapping::default(),
         tip: Tip(1),
         signature: TransactionSignature(vec![StarkFelt::ONE, StarkFelt::TWO]),
         nonce: Nonce(stark_felt!("0x1")),
@@ -39,9 +30,9 @@ fn create_declare_v3() -> ExternalDeclareTransaction {
     })
 }
 
-fn create_deploy_account_v3() -> ExternalDeployAccountTransaction {
-    ExternalDeployAccountTransaction::V3(ExternalDeployAccountTransactionV3 {
-        resource_bounds: create_resource_bounds(),
+fn create_deploy_account_v3() -> RPCDeployAccountTransaction {
+    RPCDeployAccountTransaction::V3(RPCDeployAccountTransactionV3 {
+        resource_bounds: ResourceBoundsMapping::default(),
         tip: Tip::default(),
         contract_address_salt: ContractAddressSalt(stark_felt!("0x23")),
         class_hash: ClassHash(stark_felt!("0x2")),
@@ -54,9 +45,9 @@ fn create_deploy_account_v3() -> ExternalDeployAccountTransaction {
     })
 }
 
-fn create_invoke_v3() -> ExternalInvokeTransaction {
-    ExternalInvokeTransaction::V3(ExternalInvokeTransactionV3 {
-        resource_bounds: create_resource_bounds(),
+fn create_invoke_v3() -> RPCInvokeTransaction {
+    RPCInvokeTransaction::V3(RPCInvokeTransactionV3 {
+        resource_bounds: ResourceBoundsMapping::default(),
         tip: Tip(50),
         calldata: Calldata(Arc::new(vec![stark_felt!("0x2000"), stark_felt!("0x1000")])),
         sender_address: contract_address!("0x53"),
@@ -70,11 +61,11 @@ fn create_invoke_v3() -> ExternalInvokeTransaction {
 }
 
 #[rstest]
-#[case(ExternalTransaction::Declare(create_declare_v3()))]
-#[case(ExternalTransaction::DeployAccount(create_deploy_account_v3()))]
-#[case(ExternalTransaction::Invoke(create_invoke_v3()))]
-fn test_external_transactions(#[case] tx: ExternalTransaction) {
+#[case(RPCTransaction::Declare(create_declare_v3()))]
+#[case(RPCTransaction::DeployAccount(create_deploy_account_v3()))]
+#[case(RPCTransaction::Invoke(create_invoke_v3()))]
+fn test_rpc_transactions(#[case] tx: RPCTransaction) {
     let serialized = serde_json::to_string(&tx).unwrap();
-    let deserialized: ExternalTransaction = serde_json::from_str(&serialized).unwrap();
+    let deserialized: RPCTransaction = serde_json::from_str(&serialized).unwrap();
     assert_eq!(tx, deserialized);
 }
