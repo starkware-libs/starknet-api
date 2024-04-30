b657844af8d5320ca4284331d29ef9efd438d1a1
Ayelet Zilber <ayelet@starkware.co>
2024-04-30 15:30:28 +0300

    Add a tip getter for internal transaction

--- src/internal_transaction.rs
+++ src/internal_transaction.rs
@@ -1,7 +1,7 @@
 use crate::core::{ContractAddress, Nonce};
 use crate::state::ContractClass;
 use crate::transaction::{
-    DeclareTransaction, DeployAccountTransaction, InvokeTransaction, TransactionHash,
+    DeclareTransaction, DeployAccountTransaction, InvokeTransaction, Tip, TransactionHash,
 };
 
 /// Represents a paid Starknet transaction.
@@ -26,6 +26,23 @@ impl InternalTransaction {
             InternalTransaction::Declare(tx_data) => tx_data.tx.nonce(),
             InternalTransaction::DeployAccount(tx_data) => tx_data.tx.nonce(),
             InternalTransaction::Invoke(tx_data) => tx_data.tx.nonce(),
+        }
+    }
+
+    pub fn tip(&self) -> Option<Tip> {
+        match self {
+            InternalTransaction::Declare(declare_tx) => match &declare_tx.tx {
+                DeclareTransaction::V3(tx_v3) => Some(tx_v3.tip),
+                _ => None,
+            },
+            InternalTransaction::DeployAccount(deploy_account_tx) => match &deploy_account_tx.tx {
+                DeployAccountTransaction::V3(tx_v3) => Some(tx_v3.tip),
+                _ => None,
+            },
+            InternalTransaction::Invoke(invoke_tx) => match &invoke_tx.tx {
+                InvokeTransaction::V3(tx_v3) => Some(tx_v3.tip),
+                _ => None,
+            },
         }
     }
 }
