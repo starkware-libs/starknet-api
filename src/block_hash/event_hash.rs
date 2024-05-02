28f3c5fc76aa883688cc0944fa2a989034819c0a
Yoav Gross <yoav@starkware.co>
2024-05-02 16:02:18 +0300

    Use HashChain for event hash

--- src/block_hash/event_hash.rs
+++ src/block_hash/event_hash.rs
@@ -1,5 +1,6 @@
-use crate::block_hash::utils::hash_array_preimage;
-use crate::hash::{poseidon_hash_array, PoseidonHash};
+use crate::core::EventCommitment;
+use crate::crypto::HashChain;
+use crate::hash::StarkFelt;
 use crate::transaction::{Event, TransactionHash};
 
 #[cfg(test)]
@@ -11,15 +12,15 @@ mod event_hash_test;
 ///    num_keys, key0, key1, ...,
 ///    num_contents, content0, content1, ...
 /// ).
-pub fn calculate_event_hash(event: &Event, transaction_hash: &TransactionHash) -> PoseidonHash {
-    let keys = &event.content.keys.iter().map(|k| k.0).collect::<Vec<_>>();
+pub fn calculate_event_hash(event: &Event, transaction_hash: &TransactionHash) -> EventCommitment {
+    let keys = &event.content.keys.iter().map(|k| k.0).collect::<Vec<StarkFelt>>();
     let data = &event.content.data.0;
-    poseidon_hash_array(
-        &[
-            vec![*event.from_address.0.key(), transaction_hash.0],
-            hash_array_preimage(keys),
-            hash_array_preimage(data),
-        ]
-        .concat(),
+    EventCommitment(
+        HashChain::new()
+            .chain(event.from_address.0.key())
+            .chain(&transaction_hash.0)
+            .chain_size_and_elements(keys)
+            .chain_size_and_elements(data)
+            .get_poseidon_hash(),
     )
 }
--- src/block_hash/utils.rs
+++
@@ -1,5 +0,0 @@
-use crate::hash::StarkFelt;
-
-pub(crate) fn hash_array_preimage(array: &[StarkFelt]) -> Vec<StarkFelt> {
-    [vec![array.len().into()], array.to_vec()].concat()
-}
