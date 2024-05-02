a1fc8c755ff769352b3e19be1f27d2b5b2da3cd4
Yoav Gross <yoav@starkware.co>
2024-05-02 12:54:38 +0300

    feat: calculate transaction commitment root
    commit-id:094d898f

---
+++ src/block_hash/transaction_commitment_test.rs
@@ -0,0 +1,40 @@
+use crate::block_hash::transaction_commitment::{
+    calculate_transaction_leaf, calculate_transactions_commitment, TransactionLeafElements,
+};
+use crate::core::TransactionCommitment;
+use crate::hash::StarkFelt;
+use crate::transaction::{TransactionHash, TransactionSignature};
+
+#[test]
+fn test_transaction_leaf_regression() {
+    let transaction_hash = TransactionHash(StarkFelt::ONE);
+    let transaction_signature = TransactionSignature(vec![StarkFelt::TWO, StarkFelt::THREE]);
+    let transaction_leaf_elements =
+        TransactionLeafElements { transaction_hash, transaction_signature };
+
+    let expected_leaf =
+        StarkFelt::try_from("0x2f0d8840bcf3bc629598d8a6cc80cb7c0d9e52d93dab244bbf9cd0dca0ad082")
+            .unwrap();
+
+    assert_eq!(expected_leaf, calculate_transaction_leaf(&transaction_leaf_elements));
+}
+
+#[test]
+fn test_transactions_commitment_regression() {
+    let transaction_hash = TransactionHash(StarkFelt::ONE);
+    let transaction_signature = TransactionSignature(vec![StarkFelt::TWO, StarkFelt::THREE]);
+    let transaction_leaf_elements =
+        TransactionLeafElements { transaction_hash, transaction_signature };
+
+    let expected_root =
+        StarkFelt::try_from("0x0282b635972328bd1cfa86496fe920d20bd9440cd78ee8dc90ae2b383d664dcf")
+            .unwrap();
+
+    assert_eq!(
+        TransactionCommitment(expected_root),
+        calculate_transactions_commitment(&[
+            transaction_leaf_elements.clone(),
+            transaction_leaf_elements
+        ])
+    );
+}
