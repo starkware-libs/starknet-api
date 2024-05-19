d4f5e940585443acad3286a5347eda0255a340ed
Yoav Gross <yoav@starkware.co>
2024-05-19 14:10:40 +0300

    fix: calculate l1 gas consumed

--- src/block_hash/receipt_commitment.rs
+++ src/block_hash/receipt_commitment.rs
@@ -1,25 +1,36 @@
+use crate::block::{GasPrice, GasPricePerToken};
 use crate::core::ReceiptCommitment;
 use crate::crypto::patricia_hash::calculate_root;
 use crate::crypto::utils::HashChain;
 use crate::hash::{starknet_keccak_hash, HashFunction, StarkFelt};
 use crate::transaction::{
-    ExecutionResources, MessageToL1, TransactionExecutionStatus, TransactionReceipt,
+    ExecutionResources, Fee, MessageToL1, TransactionExecutionStatus, TransactionReceipt,
+    TransactionVersion,
 };
 
 #[cfg(test)]
 #[path = "receipt_commitment_test.rs"]
 mod receipt_commitment_test;
 
-// TODO(yoav): Get these values from versioned constants.
-const L1_GAS_PER_STEP: u64 = 1;
-const L1_GAS_PER_BUILTIN_INSTANCE: u64 = 1;
-
 /// Returns the root of a Patricia tree where each leaf is a receipt hash.
 pub fn calculate_receipt_commitment<H: HashFunction>(
     transactions_receipt: &[TransactionReceipt],
+    transaction_version: &TransactionVersion,
+    l1_data_gas_price_per_token: GasPricePerToken,
+    l1_gas_price_per_token: GasPricePerToken,
 ) -> ReceiptCommitment {
     ReceiptCommitment(calculate_root::<H>(
-        transactions_receipt.iter().map(calculate_receipt_hash).collect(),
+        transactions_receipt
+            .iter()
+            .map(|receipt| {
+                calculate_receipt_hash(
+                    receipt,
+                    transaction_version,
+                    l1_data_gas_price_per_token,
+                    l1_gas_price_per_token,
+                )
+            })
+            .collect(),
     ))
 }
 
@@ -27,14 +38,27 @@ pub fn calculate_receipt_commitment<H: HashFunction>(
 //    transaction hash, amount of fee paid, hash of messages sent, revert reason,
 //    execution resources
 // ).
-fn calculate_receipt_hash(transaction_receipt: &TransactionReceipt) -> StarkFelt {
+fn calculate_receipt_hash(
+    transaction_receipt: &TransactionReceipt,
+    transaction_version: &TransactionVersion,
+    l1_data_gas_price_per_token: GasPricePerToken,
+    l1_gas_price_per_token: GasPricePerToken,
+) -> StarkFelt {
+    let l1_gas_price = get_price_by_version(l1_gas_price_per_token, transaction_version);
+    let l1_data_gas_price = get_price_by_version(l1_data_gas_price_per_token, transaction_version);
     let hash_chain = HashChain::new()
         .chain(&transaction_receipt.transaction_hash)
         .chain(&transaction_receipt.output.actual_fee().0.into())
         .chain(&calculate_messages_sent_hash(transaction_receipt.output.messages_sent()))
         .chain(&get_revert_reason_hash(transaction_receipt.output.execution_status()));
-    chain_execution_resources(hash_chain, transaction_receipt.output.execution_resources())
-        .get_poseidon_hash()
+    chain_execution_resources(
+        hash_chain,
+        transaction_receipt.output.execution_resources(),
+        transaction_receipt.output.actual_fee(),
+        l1_data_gas_price,
+        l1_gas_price,
+    )
+    .get_poseidon_hash()
 }
 
 // Poseidon(
@@ -66,18 +90,32 @@ fn get_revert_reason_hash(execution_status: &TransactionExecutionStatus) -> Star
 // Chains:
 // L2 gas consumed (In the current RPC: always 0),
 // L1 gas consumed (In the current RPC:
-//      L1 gas consumed for calldata + L1 gas consumed for steps and builtins),
+//      L1 gas consumed for calldata + L1 gas consumed for steps and builtins.
+//      Calculated as: (actual_fee - actual_l1_data_gas_fee) / l1_gas_price
 // L1 data gas consumed (In the current RPC: L1 data gas consumed for blob).
 fn chain_execution_resources(
     hash_chain: HashChain,
     execution_resources: &ExecutionResources,
+    actual_fee: Fee,
+    l1_data_gas_price: GasPrice,
+    l1_gas_price: GasPrice,
 ) -> HashChain {
-    let l1_gas_consumed = execution_resources.da_l1_gas_consumed
-        + execution_resources.steps * L1_GAS_PER_STEP
-        + execution_resources.builtin_instance_counter.values().sum::<u64>()
-            * L1_GAS_PER_BUILTIN_INSTANCE;
+    let l1_gas_consumed: u128 = (actual_fee.0
+        - (l1_data_gas_price.0) * u128::from(execution_resources.da_l1_data_gas_consumed))
+        / l1_gas_price.0;
     hash_chain
         .chain(&StarkFelt::ZERO) // L2 gas consumed
         .chain(&l1_gas_consumed.into())
         .chain(&execution_resources.da_l1_data_gas_consumed.into())
+}
+
+fn get_price_by_version(
+    price_per_token: GasPricePerToken,
+    transaction_version: &TransactionVersion,
+) -> GasPrice {
+    if transaction_version >= &TransactionVersion::THREE {
+        price_per_token.price_in_fri
+    } else {
+        price_per_token.price_in_wei
+    }
 }
